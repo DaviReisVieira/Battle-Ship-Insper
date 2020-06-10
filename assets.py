806e7f64de310e02aa0407075aa864fbac60922b
DaviReisVieira <vieira08davi38@gmail.com>
2020-06-10 12:53:12 -0300

    Corrigido Bug de Tiros e adicionado novas imagens

--- assets.py
+++ assets.py
@@ -1,5 +1,5 @@
 import pygame
-from config import WIDTH, HEIGHT, ASTEROID_WIDTH, ASTEROID_HEIGHT, SHIP_SIZE, BLACK
+from config import *
 from os import path
 
 pygame.init()
@@ -10,7 +10,7 @@ def load_assets():
     assets['background'] = pygame.transform.scale(assets['background'], (WIDTH,HEIGHT))
     assets['Letreiro'] = pygame.image.load('resources/img/TelaInicialLetreiro.png').convert_alpha()
     assets['PressKey'] = pygame.image.load('resources/img/TelaInicialPressKey.png').convert_alpha()
-    assets['asteroids'] = pygame.image.load('resources/img/asteroids.png').convert_alpha()
+    assets['asteroids'] = pygame.image.load('resources/img/asteroid.png').convert_alpha()
     assets['asteroids'] = pygame.transform.scale(assets['asteroids'], (ASTEROID_WIDTH,ASTEROID_HEIGHT))
     assets['ship1'] = pygame.image.load('resources/img/ship1.png').convert_alpha()
     assets['ship1'] = pygame.transform.scale(assets['ship1'], (SHIP_SIZE, SHIP_SIZE))
@@ -18,5 +18,6 @@ def load_assets():
     assets['ship2'] = pygame.image.load('resources/img/ship2.png').convert_alpha()
     assets['ship2'] = pygame.transform.scale(assets['ship2'], (SHIP_SIZE, SHIP_SIZE))
     assets['ship2'] = pygame.transform.rotate(assets['ship2'], 90)
-    assets['laser'] = pygame.image.load('resources/img/bullet.png').convert_alpha()
+    assets['laser1'] = pygame.image.load('resources/img/bullet1.png').convert_alpha()
+    assets['laser2'] = pygame.image.load('resources/img/bullet2.png').convert_alpha()
     return assets
